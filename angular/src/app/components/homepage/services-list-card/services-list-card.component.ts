2c9ee76491ecb6e6dd30d4bb713706da298aec77
NHTNhi <1712117@student.hcmus.edu.vn>
2020-12-14 02:58:11 +0700

    feat: add services cards

---
+++ angular/src/app/components/homepage/services-list-card/services-list-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-services-list-card',
+  templateUrl: './services-list-card.component.html',
+  styleUrls: ['./services-list-card.component.scss']
+})
+export class ServicesListCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
